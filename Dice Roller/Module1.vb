17b0d4d0f0e64c75563871bc82920b7e36de2747
Taylor Herndon <TaylorBryanHerndon@gmail.com>
2021-01-13 17:40:41 -0700

    Add project files.

---
+++ Dice Roller/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim Input = 0
+
+        Console.WriteLine("Welcome to DiceRoller™ would you like to create a new character?")
+        Console.WriteLine("Enter Y to roll up a new character")
+
+
+
+    End Sub
+
+End Module
